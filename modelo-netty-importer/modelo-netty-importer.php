abee277aacc522ead7a2abab54e4363bec8ea360
Etienne Zulauf <etienne.zulauf@gmail.com>
2026-06-08 12:19:13 +0200

    fix: correct Plugin URI (double Modelo typo)

--- modelo-netty-importer/modelo-netty-importer.php
+++ modelo-netty-importer/modelo-netty-importer.php
@@ -5,7 +5,7 @@
  * Version: 1.1.0
  * Author: Ethersys
  * Author URI: https://www.ethersys.fr
- * Plugin URI: https://github.com/ethersys/Modelo-Modelo-Netty-Importer
+ * Plugin URI: https://github.com/ethersys/Modelo-Netty-Importer
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: modelo-netty-importer
